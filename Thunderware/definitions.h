66498ca9a4ccb0f8985498b6386036eaccdf4433
Matt Rogge <mprogge@gmail.com>
2019-11-14 21:48:08 -0800

    Merge branch 'Thunderboard-Compatable' of https://github.com/TechforTrade/Thunderhead-Filament-Extruder into Thunderboard-Compatable

---
+++ Thunderware/definitions.h
@@ -0,0 +1,2 @@
+#define OLD_PINS //uncomment this if using an extruder that DOES NOT have a Thunderboard PCB
+#define MAC_OUTFEED // uncomment this inorder to have the outfeed directly controlled by the main arduino controller
